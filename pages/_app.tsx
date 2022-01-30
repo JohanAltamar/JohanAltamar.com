5125608b0ec26b0a3de83b78a93cf3de62e23724
Johan Altamar <johan.altamar.dev@gmail.com>
2022-01-30 16:11:29 -0500

    Add(-): Initial config added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
+/**
+ * This is the main entry point for the application.
+ * @return {JSX.Element}
+ */
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
